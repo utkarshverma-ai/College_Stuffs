6e61805af33c8a0154f557af333c4e38c25b68ef
utkarshverma-ai <codexutkarsh@gmail.com>
2026-02-04 15:18:01 +0530

    Heyy

--- file.js
+++ file.js
@@ -1,11 +1,30 @@
-import add from './utils.js'; // for default export
-import { multiply, divide } from './utils.js'; // for named exports
+// import add from './utils.js'; // for default export
+// import { multiply, divide } from './utils.js'; // for named exports
 
-const output = add(2, 3);
-console.log("Output:", output);
+// const output = add(2, 3);
+// console.log("Output:", output);
 
-const product = multiply(4, 5);
-console.log("Product:", product);
+// const product = multiply(4, 5);
+// console.log("Product:", product);
 
-const quotient = divide(10, 2);
-console.log("Quotient:", quotient);
+// const quotient = divide(10, 2);
+// console.log("Quotient:", quotient);
+
+
+import fs from'fs';
+import path from'path';
+
+const cpPath = path.join(  
+    "functions",
+    "SampleFolder",
+    "..",
+    "function.js"
+);
+
+console.log("crossPlatform path name:", cpPath);
+
+fs.readFile(cpPath, 'utf8', (err, data) => {
+    console.log("err:" ,err);
+    console.log("data:", data);
+});   
+    
