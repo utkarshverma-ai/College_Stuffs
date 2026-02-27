b96b7f8d2f6ba324a13aa090e4f74f1ee30ad259
utkarshverma-ai <codexutkarsh@gmail.com>
2026-02-27 11:32:24 +0530

    Updated

--- file3.js
+++ file3.js
@@ -23,4 +23,8 @@ const server = http.createServer((req, res) => {
 
 server.listen(3000, () => {
     console.log("Server running on port 3000");
+    console.log("Now I am feeling better");
+    console.log("Server is also called as Instance");
+    
+    
 });
